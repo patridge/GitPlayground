8b16a445981b3591bfc9c8243bef4a9ff9e2df46
Adam Patridge <patridge@gmail.com>
2017-07-25 07:47:43 -0600

    First run at getting info from GitHub (via personal access token in ignored config override file)

--- GitPlayground/Program.cs
+++ GitPlayground/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Nito.AsyncEx;
+using Octokit;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +11,29 @@ namespace GitPlayground
 {
     class Program
     {
+        static string _GitHubPersonalAccessToken;
+        static string GitHubPersonalAccessToken
+        {
+            get {
+                const string configKey = "gitHubPersonalAccessToken";
+                return _GitHubPersonalAccessToken ?? (_GitHubPersonalAccessToken = ConfigurationManager.AppSettings.Get(configKey));
+            }
+        }
         static void Main(string[] args)
         {
+            AsyncContext.Run(() => MainAsync(args));
+            Console.Read();
+        }
+
+        static async void MainAsync(string[] args)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("submodule-helper"));
+            var tokenAuth = new Credentials(GitHubPersonalAccessToken);
+            client.Credentials = tokenAuth;
+
+            var user = await client.User.Current();
+
+            Console.WriteLine($"{user.Name}");
         }
     }
 }
